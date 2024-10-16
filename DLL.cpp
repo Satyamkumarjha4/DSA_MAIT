c242643657ed9d4de1438b6bf62bce30e84beb42
Satyamkumarjha4 <satyamjha4@gmail.com>
2024-10-16 14:43:40 +0530

    DLL

---
+++ DLL.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val) : data(val), next(nullptr), prev(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+
+public:
+    DoublyLinkedList() : head(nullptr) {}
+
+    void append(int data) {
+        Node* newNode = new Node(data);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+
+    ~DoublyLinkedList() {
+        Node* temp = head;
+        while (temp) {
+            Node* next = temp->next;
+            delete temp;
+            temp = next;
+        }
+    }
+};
+
+int main() {
+    DoublyLinkedList dll;
+    dll.append(10);
+    dll.append(20);
+    dll.append(30);
+
+    std::cout << "Doubly Linked List: ";
+    dll.display();
+
+    return 0;
+}
